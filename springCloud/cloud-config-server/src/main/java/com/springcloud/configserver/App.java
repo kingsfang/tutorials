d574f8f89c4dc7180d645fb192894fa544589c83
zengpeng <379293139@qq.com>
2017-07-27 21:10:23 +0800

    C

---
+++ springCloud/cloud-config-server/src/main/java/com/springcloud/configserver/App.java
@@ -0,0 +1,16 @@
+package com.springcloud.configserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zengpeng on 2017/7/2.
+ */
+@SpringBootApplication
+public class App {
+
+
+    public static void main(String... args) {
+
+    }
+
+}
